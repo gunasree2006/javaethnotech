9f41fd104577b7e07faf04f64dd626b7ba1644ae
gunasree2006 <pullareddygarigunasree@gmail.com>
2026-06-15 14:09:47 +0530

    Add files via upload

---
+++ collection/diff_betw_capacityandsize.java
@@ -0,0 +1,12 @@
+package collections.arraylist;
+
+import java.util.ArrayList;
+public class diff_betw_capacityandsize {
+
+        public static void main(String args[]){
+        ArrayList<Integer> list=new ArrayList<>(10);
+        System.out.println(list.size());
+    }
+
+
+}
