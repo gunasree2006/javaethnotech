d43e12be2f5af8c023d615036096c3fbbf24257f
gunasree2006 <pullareddygarigunasree@gmail.com>
2026-06-18 10:46:07 +0530

    Add files via upload

---
+++ generics/bounded/limit_for_numbers.java
@@ -0,0 +1,11 @@
+package generics.project.boundedgenerics.content;
+
+public class limit_for_numbers <T extends Number>{
+    T digit;
+    public void setDigit(T name){
+        this.digit=digit;
+    }
+    public T getDigit(){
+        return digit;
+    }
+}
