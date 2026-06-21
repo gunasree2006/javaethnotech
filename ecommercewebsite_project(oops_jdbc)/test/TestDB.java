80ea4b9f0975283a50a3db286c50f194a67a508e
gunasree2006 <pullareddygarigunasree@gmail.com>
2026-06-21 17:18:34 +0530

    Create TestDB.java

---
+++ ecommercewebsite_project(oops_jdbc)/test/TestDB.java
@@ -0,0 +1,9 @@
+package Ecommerce_project.test;
+
+import Ecommerce_project.util.DBConnection;
+
+public class TestDB {
+    public static void main(String[] args) {
+        DBConnection.getConnection();
+    }
+}
